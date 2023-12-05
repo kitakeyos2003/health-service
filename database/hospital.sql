e24122f2b7f1292baebb7fd8eca026719478d681
Hoàng Hữu Dũng <kitakeyos@gmail.com>
2023-12-05 21:04:09 +0700

    initial project

---
+++ database/hospital.sql
@@ -0,0 +1,258 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1:3306
+-- Thời gian đã tạo: Th12 05, 2023 lúc 02:00 PM
+-- Phiên bản máy phục vụ: 10.6.5-MariaDB
+-- Phiên bản PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `aemail` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `apassword` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  PRIMARY KEY (`aemail`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `admin`
+--
+
+INSERT INTO `admin` (`aemail`, `apassword`) VALUES
+('admin@edoc.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+CREATE TABLE IF NOT EXISTS `appointment` (
+  `appoid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(10) DEFAULT NULL,
+  `apponum` int(3) DEFAULT NULL,
+  `scheduleid` int(10) DEFAULT NULL,
+  `appodate` date DEFAULT NULL,
+  PRIMARY KEY (`appoid`),
+  KEY `pid` (`pid`),
+  KEY `scheduleid` (`scheduleid`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `appointment`
+--
+
+INSERT INTO `appointment` (`appoid`, `pid`, `apponum`, `scheduleid`, `appodate`) VALUES
+(1, 1, 1, 1, '2022-06-03'),
+(2, 3, 2, 1, '2023-12-05');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+CREATE TABLE IF NOT EXISTS `doctor` (
+  `docid` int(11) NOT NULL AUTO_INCREMENT,
+  `docemail` varchar(255) DEFAULT NULL,
+  `docname` varchar(255) DEFAULT NULL,
+  `docpassword` varchar(255) DEFAULT NULL,
+  `docnic` varchar(15) DEFAULT NULL,
+  `doctel` varchar(15) DEFAULT NULL,
+  `specialties` int(2) DEFAULT NULL,
+  PRIMARY KEY (`docid`),
+  KEY `specialties` (`specialties`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `doctor`
+--
+
+INSERT INTO `doctor` (`docid`, `docemail`, `docname`, `docpassword`, `docnic`, `doctel`, `specialties`) VALUES
+(1, 'doctor@edoc.com', 'Test Doctor', '123', '000000000', '0110000000', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+CREATE TABLE IF NOT EXISTS `patient` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `pemail` varchar(255) DEFAULT NULL,
+  `pname` varchar(255) DEFAULT NULL,
+  `ppassword` varchar(255) DEFAULT NULL,
+  `paddress` varchar(255) DEFAULT NULL,
+  `pnic` varchar(15) DEFAULT NULL,
+  `pdob` date DEFAULT NULL,
+  `ptel` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`pid`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `patient`
+--
+
+INSERT INTO `patient` (`pid`, `pemail`, `pname`, `ppassword`, `paddress`, `pnic`, `pdob`, `ptel`) VALUES
+(1, 'patient@edoc.com', 'Test Patient', '123', 'Sri Lanka', '0000000000', '2000-01-01', '0120000000'),
+(2, 'emhashenudara@gmail.com', 'Hashen Udara', '123', 'Sri Lanka', '0110000000', '2022-06-03', '0700000000'),
+(3, 'kitakeyos@gmail.com', 'DÅ©ng HoÃ ng Há»¯u', 'dungdz', 'ThÆ° ÄÃ´i, Nguyá»‡t Äá»©c, Thuáº­n ThÃ nh', '123567890', '2003-02-23', '0348658498');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `schedule`
+--
+
+DROP TABLE IF EXISTS `schedule`;
+CREATE TABLE IF NOT EXISTS `schedule` (
+  `scheduleid` int(11) NOT NULL AUTO_INCREMENT,
+  `docid` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `scheduledate` date DEFAULT NULL,
+  `scheduletime` time DEFAULT NULL,
+  `nop` int(4) DEFAULT NULL,
+  PRIMARY KEY (`scheduleid`),
+  KEY `docid` (`docid`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `schedule`
+--
+
+INSERT INTO `schedule` (`scheduleid`, `docid`, `title`, `scheduledate`, `scheduletime`, `nop`) VALUES
+(1, '1', 'Test Session', '2050-01-01', '18:00:00', 50),
+(2, '1', '1', '2022-06-10', '20:36:00', 1),
+(3, '1', '12', '2022-06-10', '20:33:00', 1),
+(4, '1', '1', '2022-06-10', '12:32:00', 1),
+(5, '1', '1', '2022-06-10', '20:35:00', 1),
+(6, '1', '12', '2022-06-10', '20:35:00', 1),
+(7, '1', '1', '2022-06-24', '20:36:00', 1),
+(8, '1', '12', '2022-06-10', '13:33:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `specialties`
+--
+
+DROP TABLE IF EXISTS `specialties`;
+CREATE TABLE IF NOT EXISTS `specialties` (
+  `id` int(2) NOT NULL,
+  `sname` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `specialties`
+--
+
+INSERT INTO `specialties` (`id`, `sname`) VALUES
+(1, 'Accident and emergency medicine'),
+(2, 'Allergology'),
+(3, 'Anaesthetics'),
+(4, 'Biological hematology'),
+(5, 'Cardiology'),
+(6, 'Child psychiatry'),
+(7, 'Clinical biology'),
+(8, 'Clinical chemistry'),
+(9, 'Clinical neurophysiology'),
+(10, 'Clinical radiology'),
+(11, 'Dental, oral and maxillo-facial surgery'),
+(12, 'Dermato-venerology'),
+(13, 'Dermatology'),
+(14, 'Endocrinology'),
+(15, 'Gastro-enterologic surgery'),
+(16, 'Gastroenterology'),
+(17, 'General hematology'),
+(18, 'General Practice'),
+(19, 'General surgery'),
+(20, 'Geriatrics'),
+(21, 'Immunology'),
+(22, 'Infectious diseases'),
+(23, 'Internal medicine'),
+(24, 'Laboratory medicine'),
+(25, 'Maxillo-facial surgery'),
+(26, 'Microbiology'),
+(27, 'Nephrology'),
+(28, 'Neuro-psychiatry'),
+(29, 'Neurology'),
+(30, 'Neurosurgery'),
+(31, 'Nuclear medicine'),
+(32, 'Obstetrics and gynecology'),
+(33, 'Occupational medicine'),
+(34, 'Ophthalmology'),
+(35, 'Orthopaedics'),
+(36, 'Otorhinolaryngology'),
+(37, 'Paediatric surgery'),
+(38, 'Paediatrics'),
+(39, 'Pathology'),
+(40, 'Pharmacology'),
+(41, 'Physical medicine and rehabilitation'),
+(42, 'Plastic surgery'),
+(43, 'Podiatric Medicine'),
+(44, 'Podiatric Surgery'),
+(45, 'Psychiatry'),
+(46, 'Public health and Preventive Medicine'),
+(47, 'Radiology'),
+(48, 'Radiotherapy'),
+(49, 'Respiratory medicine'),
+(50, 'Rheumatology'),
+(51, 'Stomatology'),
+(52, 'Thoracic surgery'),
+(53, 'Tropical medicine'),
+(54, 'Urology'),
+(55, 'Vascular surgery'),
+(56, 'Venereology');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `webuser`
+--
+
+DROP TABLE IF EXISTS `webuser`;
+CREATE TABLE IF NOT EXISTS `webuser` (
+  `email` varchar(255) NOT NULL,
+  `usertype` char(1) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `webuser`
+--
+
+INSERT INTO `webuser` (`email`, `usertype`) VALUES
+('admin@edoc.com', 'a'),
+('doctor@edoc.com', 'd'),
+('patient@edoc.com', 'p'),
+('emhashenudara@gmail.com', 'p'),
+('kitakeyos@gmail.com', 'p');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
